42afcf3d811cf2c9d5623d0a0f749d78aa6ce1a0
Lancelot Normand <lancelotnormand@outlook.com>
2024-04-28 12:57:09 -0400

    add Slurm script for different clusters

---
+++ recipes/finetuning/run_narval.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+module load cuda python/3.10 arrow/14.0.0
+export OMP_NUM_THREADS=4
+source ~/llama_env/bin/activate
+torchrun --nnodes=1 --nproc_per_node 4 --rdzv_id 69 --rdzv_backend c10d --rdzv_endpoint $1:29500 ./finetuning.py --model_name /home/lancelot/scratch/Meta-Llama-3-8B-Instruct --output_dir /home/lancelot/scratch/peft --dataset alpaca_dataset --enable_fsdp --use_peft --peft_method lora
